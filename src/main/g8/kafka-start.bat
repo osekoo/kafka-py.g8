8dcb9f6a61eef814d58f49e34963f7124cbd9ff4
wopoadi <133659712+wopoadi@users.noreply.github.com>
2025-06-03 22:15:42 +0200

    Update kafka-start.bat

--- src/main/g8/kafka-start.bat
+++ src/main/g8/kafka-start.bat
@@ -1,4 +1,5 @@
 @echo off
 echo "Starting Kafka broker..."
 docker rm -f kafka-broker
-docker run -it --rm --name kafka-broker --user root -p 9000:9000 -p 9092:9092 -p 9093:9093 ghcr.io/osekoo/kafka:3.5
+docker network create teaching
+docker run -it --rm --name kafka-broker --network teaching --user root -p 9000:9000 -p 9092:9092 -p 9093:9093 ghcr.io/osekoo/kafka:3.5
